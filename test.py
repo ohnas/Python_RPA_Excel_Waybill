0b6c266edc5c7c5c95343792598aa4da2e32def4
ohnas <74810513+ohnas@users.noreply.github.com>
2022-01-05 14:54:04 +0900

    #5 rename and drop duplicates

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
-df = pd.read_excel("read_sample.xlsx", engine="openpyxl")
-df = df[["상품주문번호", "수취인명", "옵션정보", "수량", "수취인연락처1", "수취인연락처2", "배송지", "배송메세지"]]
+row_df = pd.read_excel("read_sample.xlsx", engine="openpyxl")
+select_df = row_df[
+    ["상품주문번호", "수취인명", "옵션정보", "수량", "수취인연락처1", "수취인연락처2", "배송지", "배송메세지"]
+]
 
-df["내품명"] = ""
-df["운임구분"] = "신용"
-df["박스타입"] = "극소"
-df["기본운임"] = 2050
-df["보내는분성명"] = "몬스터moster"
-df["보내는분주소(전체, 분할)"] = "인천 서구 가좌동 585-14 cj대한통운 인천가좌심곡대리점"
-df["보내는분전화번호"] = "1522-8145"
-df["보내는분기타연락처"] = ""
+select_df["내품명"] = ""
+select_df["운임구분"] = "신용"
+select_df["박스타입"] = "극소"
+select_df["기본운임"] = 2050
+select_df["보내는분성명"] = "몬스터moster"
+select_df["보내는분주소(전체, 분할)"] = "인천 서구 가좌동 585-14 cj대한통운 인천가좌심곡대리점"
+select_df["보내는분전화번호"] = "1522-8145"
+select_df["보내는분기타연락처"] = ""
 
-df = df[
+relocation_df = select_df[
     [
         "상품주문번호",
         "수취인명",
@@ -32,7 +34,7 @@ df = df[
         "보내는분기타연락처",
     ]
 ]
-df = df.rename(
+rename_df = relocation_df.rename(
     columns={
         "상품주문번호": "고객주문번호",
         "수취인명": "받는분성명",
@@ -44,8 +46,16 @@ df = df.rename(
     }
 )
 
+# rename_df 에서 받는분섬명 , 받는분주소, 받는분전화번호를 기준으로 중복데이터 제거한 가공된 dataframe
+drop_df = rename_df.drop_duplicates(
+    keep=False, subset=["받는분성명", "받는분주소(전체,분할)", "받는분전화번호"]
+)
 
-# 받는분섬명 , 받는분주소, 받는분전화번호를 기준으로 중복데이터 찾아내기
-df = df[df.duplicated(keep=False, subset=["받는분성명", "받는분주소(전체,분할)", "받는분전화번호"])]
 
-df.to_excel("test_sample.xlsx", index=False)
+# rename_df 에서 받는분섬명 , 받는분주소, 받는분전화번호를 기준으로 중복데이터만 골라낸 가공된 dataframe
+duplicated_df = rename_df[
+    rename_df.duplicated(keep=False, subset=["받는분성명", "받는분주소(전체,분할)", "받는분전화번호"])
+
+
+drop_df.to_excel("test_sample.xlsx", index=False)
+duplicated_df.to_excel("test_sample1.xlsx", index=False)
