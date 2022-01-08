8feaa9fc4fcca7e0df28a8d85e95b9640ba7a0e4
ohnas <74810513+ohnas@users.noreply.github.com>
2022-01-08 17:13:33 +0900

    #8 db find data

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+from os import dup
+import time
 import pymysql
 import pandas as pd
+from pymysql.cursors import DictCursor
 import DB_auth
+
+start = time.time()
 
 # db 접속정보를 DB_auth.py에서 불러와서 입력해놓기
 conn = pymysql.connect(
@@ -12,27 +17,10 @@ conn = pymysql.connect(
     charset=DB_auth.info["charset"],
 )
 
-# db에 접속해서 활동하기
-cur = conn.cursor()
-sql = "SELECT * FROM testtable"
-cur.execute(sql)
-
-conn.close()
-
-
-row_df = pd.read_excel("read_sample.xlsx", engine="openpyxl")
+row_df = pd.read_excel("read_sample1.xlsx", engine="openpyxl")
 select_df = row_df[
     ["상품주문번호", "수취인명", "옵션정보", "수량", "수취인연락처1", "수취인연락처2", "배송지", "배송메세지"]
 ]
-
-select_df["내품명"] = ""
-select_df["운임구분"] = "신용"
-select_df["박스타입"] = "극소"
-select_df["기본운임"] = 2050
-select_df["보내는분성명"] = "몬스터moster"
-select_df["보내는분주소(전체, 분할)"] = "인천 서구 가좌동 585-14 cj대한통운 인천가좌심곡대리점"
-select_df["보내는분전화번호"] = "1522-8145"
-select_df["보내는분기타연락처"] = ""
 
 relocation_df = select_df[
     [
@@ -42,16 +30,8 @@ relocation_df = select_df[
         "수취인연락처1",
         "수취인연락처2",
         "옵션정보",
-        "내품명",
         "수량",
         "배송메세지",
-        "운임구분",
-        "박스타입",
-        "기본운임",
-        "보내는분성명",
-        "보내는분주소(전체, 분할)",
-        "보내는분전화번호",
-        "보내는분기타연락처",
     ]
 ]
 rename_df = relocation_df.rename(
@@ -65,20 +45,48 @@ rename_df = relocation_df.rename(
         "수량": "내품수량",
     }
 )
+# rename_df 에서 인덱스를 고객주문번호으로 재설정하기
+set_index_df = rename_df.set_index("고객주문번호", drop=False)
 
-# rename_df 에서 받는분섬명 , 받는분주소, 받는분전화번호를 기준으로 중복데이터 제거한 가공된 dataframe
-drop_df = rename_df.drop_duplicates(
+# set_index_df 에서 받는분섬명 , 받는분주소, 받는분전화번호를 기준으로 중복데이터 제거한 가공된 dataframe
+drop_df = set_index_df.drop_duplicates(
     keep=False, subset=["받는분성명", "받는분주소(전체,분할)", "받는분전화번호"]
 )
 
 
-# rename_df 에서 받는분섬명 , 받는분주소, 받는분전화번호를 기준으로 중복데이터만 골라낸 가공된 dataframe
-duplicated_df = rename_df[
+# set_index_df 에서 받는분섬명 , 받는분주소, 받는분전화번호를 기준으로 중복데이터만 골라낸 가공된 dataframe
+""" duplicated_df = set_index_df[
     rename_df.duplicated(keep=False, subset=["받는분성명", "받는분주소(전체,분할)", "받는분전화번호"])
-]
+] """
+
+# db에 접속해서 활동하기 db에서 활동하는 cursor를 Dict cursor 로 설정
+cur = conn.cursor(pymysql.cursors.DictCursor)
+sql = "SELECT * FROM testtable"
+cur.execute(sql)
+
+while True:
+    db_row = cur.fetchone()
+    if db_row == None:
+        break
+    for df_row in drop_df.to_dict("records"):
+        if df_row["품목명"] == db_row["품목명"] and df_row["내품수량"] == db_row["내품수량"]:
+            drop_df.loc[df_row["고객주문번호"], "운임구분"] = db_row["운임구분"]
+            drop_df.loc[df_row["고객주문번호"], "박스타입"] = db_row["박스타입"]
+            drop_df.loc[df_row["고객주문번호"], "기본운임"] = db_row["기본운임"]
+            drop_df.loc[df_row["고객주문번호"], "보내는분성명"] = db_row["보내는분성명"]
+            drop_df.loc[df_row["고객주문번호"], "보내는분주소(전체, 분할)"] = db_row["보내는분주소(전체, 분할)"]
+            drop_df.loc[df_row["고객주문번호"], "보내는분전화번호"] = db_row["보내는분전화번호"]
+            drop_df.loc[df_row["고객주문번호"], "보내는분기타연락처"] = db_row["보내는분기타연락처"]
+        else:
+            continue
+
+conn.close()
+
+drop_df.to_excel("test_sample.xlsx", index=False)
+print(time.time() - start)
 
 # drop_df 와 duplicated_df 를 pd.concat 함수를 이용하여 병합하여, concat_df 로 만들어내기
-concat_df = pd.concat([drop_df, duplicated_df])
+# concat_df = pd.concat([drop_df, duplicated_df])
 
-concat_df.to_excel("test_sample.xlsx", index=False)
-duplicated_df.to_excel("test_sample1.xlsx", index=False)
+# concat_df를 엑셀파일로 만들어내기
+# concat_df.to_excel("test_sample.xlsx", index=False)
